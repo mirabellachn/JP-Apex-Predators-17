7ca0d9b1b4f53f45f1bee423fcbe745e22a769f1
mirabellachn <mirabellacn@gmail.com>
2024-11-19 00:07:41 +0700

    init commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by mirabella  on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
