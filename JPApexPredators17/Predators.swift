33ab01b6fa797acc9c82b14dfdc513e97ab2626c
mirabellachn <mirabellacn@gmail.com>
2024-11-20 20:09:26 +0700

    predators

---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,21 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by mirabella  on 19/11/24.
+//
+
+import Foundation
+
+class Predators {
+    var apexPredators: [ApexPredator] = []
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            
+        }
+        
+        let url2 = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json")
+            
+    }
+}
