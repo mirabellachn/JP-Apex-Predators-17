819e3ffc5d545facfa16186193a997938db78680
mirabellachn <mirabellacn@gmail.com>
2024-11-21 17:15:11 +0700

    decoding

--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -10,12 +10,21 @@ import Foundation
 class Predators {
     var apexPredators: [ApexPredator] = []
     
+    init() {
+        decodeApexPredatorData()
+    }
+    
     func decodeApexPredatorData() {
         if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
-            
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON Dara \(error)")
+                
+            }
         }
-        
-        let url2 = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json")
-            
     }
 }
