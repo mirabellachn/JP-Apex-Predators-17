8063deaa754df3a7701121f52fdc5d9a8b5adf93
mirabellachn <mirabellacn@gmail.com>
2024-12-04 16:54:32 +0700

    details

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
         NavigationStack {
             List(filteredDinos) { predator in
                 NavigationLink {
-                    PredatorDetail()
+                    PredatorDetail(predator: predator)
                 } label: {
                     HStack {
                         // Dinosaur Image
--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct PredatorDetail: View {
+    let predator: ApexPredator
+    
     var body: some View {
-        ScrollView {
-            
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing) {
+                    // Background Image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Dino image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                    
+                }
+                VStack(alignment: .leading) {
+                    // Dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    //Current Location
+                    
+                    
+                    // Appears in
+                    Text("Appears In:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("∙" + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    
+                    // Movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // Link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+            .ignoresSafeArea()
         }
     }
 }
 
 #Preview {
-    PredatorDetail()
+    PredatorDetail(predator: Predators().apexPredators[2])
 //        .preferredColorScheme(.dark)
 }
